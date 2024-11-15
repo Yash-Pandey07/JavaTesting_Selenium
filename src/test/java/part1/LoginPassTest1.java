1909a4454cc7d8eb2b4ce829a385e0ed1e6771ec
Yash Pandey <ypandey777@yahoo.in>
2024-11-15 18:57:14 +0530

    1st Selenium Using Java Test

--- src/test/java/part1/LoginPassTest1.java
+++ src/test/java/part1/LoginPassTest1.java
@@ -4,11 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class SeleniumTest1 {
+public class LoginPassTest1 {
     WebDriver driver;
 
     @BeforeClass    //Before the class run
@@ -20,7 +21,7 @@ public class SeleniumTest1 {
 
     @AfterClass     //After the class is run
     public void close() {
-        driver.quit();              //close the whole browser .close to close that tab
+            driver.quit();              //close the whole browser .close to close that tab
     }
 
     @Test
@@ -34,5 +35,12 @@ public class SeleniumTest1 {
         password.sendKeys("admin123");
 
         driver.findElement(By.tagName("button")).click();
+
+        //Pause after login
+        Thread.sleep(2000);
+        String actualResult = driver.findElement(By.tagName("h6")).getText();
+        String expectedResult = "Dashboard";
+
+        Assert.assertEquals(actualResult, expectedResult);
     }
 }
