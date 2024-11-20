e6deb5a416cb218ad42771311067db09ee572642
Yash Pandey <ypandey777@yahoo.in>
2024-11-20 06:42:59 +0530

    Product Test Script

---
+++ src/test/java/part2/com/saucedemo/test/products/ProductsTest.java
@@ -0,0 +1,14 @@
+package part2.com.saucedemo.test.products;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part1.com.saucedemo.pages.ProductsPage;
+import part2.com.saucedemo.base.BaseTest;
+
+public class ProductsTest extends BaseTest {
+    @Test
+    public void testProductsHeadeisDisplayed(){
+        ProductsPage productsPage = loginPage.logintoApplication("standard_user", "secret_sauce");
+        Assert.assertTrue(productsPage.isProductHeaderDisplayed(),"\n Products Header Is not displayed !\n");
+    }
+}
