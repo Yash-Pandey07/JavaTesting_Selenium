b8f7889047a65fd7c533c43933fc65c5ebe1388e
Yash Pandey <ypandey777@yahoo.in>
2024-11-22 23:48:02 +0530

    Scroll To Element Using JavaScriptExecutor

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -1,7 +1,14 @@
 package com.demoqa.pages;
 
 import com.base.BasePage;
+import com.demoqa.pages.forms.FormsPage;
+import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
+    private By formsCard = By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[2]/div/div[3]/h5");
 
+    public FormsPage goToForms() {
+        click(formsCard);
+        return new FormsPage();
+    }
 }
---
+++ src/test/java/part3/com/demoqa/tests/part3/javascript/JavaScriptTest.java
@@ -0,0 +1,13 @@
+package part3.com.demoqa.tests.part3.javascript;
+
+import org.testng.annotations.Test;
+import org.testng.internal.BaseTestMethod;
+import part3.com.demoqa.base.Base;
+
+public class JavaScriptTest extends Base {
+
+    @Test
+    public void testScrollingToElement() {
+
+    }
+}
