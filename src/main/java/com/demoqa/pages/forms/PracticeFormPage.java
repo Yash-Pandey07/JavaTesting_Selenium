aed7781d67064339fc62e9010b88ac9518c9dbb1
Yash Pandey <ypandey777@yahoo.in>
2024-11-25 23:45:29 +0530

    Working With Checkboxes

--- src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
+++ src/main/java/com/demoqa/pages/forms/PracticeFormPage.java
@@ -6,6 +6,10 @@ import static utilities.JavaScriptUtility.*;
 
 public class PracticeFormPage extends FormsPage {
     private By femaleRadioButton = By.id("gender-radio-2");
+    private By sportsHobbyCheckbox = By.id("hobbies-checkbox-1");
+    private By readingHobbyCheckbox = By.id("hobbies-checkbox-2");
+    private By musicHobbyCheckbox = By.id("hobbies-checkbox-3");
+    private By submitButton = By.id("submit");
 
     public void clickFemaleRadioButton() {
         scrollToElementJS(femaleRadioButton);
@@ -14,5 +18,42 @@ public class PracticeFormPage extends FormsPage {
 
     public boolean isFemaleSelected() {
         return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSportsCheckbox() {
+        if (!find(sportsHobbyCheckbox).isSelected()) {
+            scrollToElementJS(sportsHobbyCheckbox);
+            clickJS(sportsHobbyCheckbox);
+        }
+    }
+
+    public void clickReadingCheckbox() {
+        if (!find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public void clickMusicCheckbox() {
+        if (!find(musicHobbyCheckbox).isSelected()) {
+            scrollToElementJS(musicHobbyCheckbox);
+            clickJS(musicHobbyCheckbox);
+        }
+    }
+
+    public void unclickReadingCheckbox() {
+        if (find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public boolean isReadingSelected() {
+        return find(readingHobbyCheckbox).isSelected();
+    }
+
+    public void clickSubmitButton() {
+//    scrollToElementJS(submitButton);
+        click(submitButton);
     }
 }
