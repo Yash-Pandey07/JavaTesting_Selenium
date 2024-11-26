020289c8464bf70f6b4fe412599acd410f34142e
Yash Pandey <ypandey777@yahoo.in>
2024-11-26 23:53:25 +0530

    Working With Tables

---
+++ src/test/java/part3/com/demoqa/tests/part3/elements/WebTablesTest.java
@@ -0,0 +1,29 @@
+package part3.com.demoqa.tests.part3.elements;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3.com.demoqa.base.BaseTest;
+
+public class WebTablesTest extends BaseTest {
+
+    @Test
+    public void testWebTable() {
+        String email = "kierra@example.com";
+        String expectedAge = "34";
+
+        var webTablePage = homePage.goToElements().clickWebTables();
+        webTablePage.clickEdit(email);
+        webTablePage.setAge("34");
+        webTablePage.clickSubmitButton();
+        String actualAge = webTablePage.getTableAge(email);
+//        Assert.assertEquals(actualAge, expectedAge,
+//                "\n Actual & Expected Ages Do Not Match \n");
+
+        // Check if the ages are equal and log a message
+        if (actualAge.equals(expectedAge)) {
+            System.out.println("Actual & Expected Ages Match: " + actualAge);
+        } else {
+            Assert.assertEquals(actualAge, expectedAge, "\n Actual & Expected Ages Do Not Match \n");
+        }
+    }
+}
